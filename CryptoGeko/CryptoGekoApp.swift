f0807bac71956ea5860034bd3cf878cf40c4574a
misaki1301 <paul.frankpc@gmail.com>
2023-11-19 10:54:05 -0500

    Initial Commit

---
+++ CryptoGeko/CryptoGekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoGekoApp.swift
+//  CryptoGeko
+//
+//  Created by Paul Frank Pacheco Carpio on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoGekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
